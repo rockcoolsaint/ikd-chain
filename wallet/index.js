9f081946bdd928d15f123dc168a1d52766f35ef3
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-09-23 17:46:13 +0100

    calculate balance with more test specifications

--- wallet/index.js
+++ wallet/index.js
@@ -19,7 +19,9 @@ class Wallet {
     return this.keyPair.sign(dataHash);
   }
 
-  createTransaction(recipient, amount, transactionPool) {
+  createTransaction(recipient, amount, blockchain, transactionPool) {
+    this.balance = this.calculateBalance(blockchain);
+
     if (amount > this.balance) {
       console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
       return;
@@ -44,7 +46,7 @@ class Wallet {
       transactions.push(transaction);
     }));
 
-    const walletInputTs = transactions.filter(transaction => transactions.input.address === this.publicKey);
+    const walletInputTs = transactions.filter(transaction => transaction.input.address === this.publicKey);
 
     let startTime = 0;
 
