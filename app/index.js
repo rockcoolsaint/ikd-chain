20031ae3c94c69566f56b220cf93a290dfe9c2c0
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-09-23 15:47:00 +0100

    create transactions endpoint

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Blockchain = require('../blockchain');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 const P2pServer = require('./p2p-server');
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(express.json());
 
@@ -38,6 +40,12 @@ app.post('/transact', (req, res) => {
   p2pServer.broadcastTransaction(transaction);
   res.redirect('/transactions');
 });
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
+})
 
 app.get('/public-key', (req, res) => {
   res.json({publicKey: wallet.publicKey});
